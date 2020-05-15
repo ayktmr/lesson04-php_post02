abcb1de9214bb4161c95c91b0147d26a89cb661f
ayktmr <ate.ayakotamura@gmail.com>
2020-05-15 14:11:02 +0900

    [add]画像部分のnoticeエラーを回避

--- join/index.php
+++ join/index.php
@@ -86,7 +86,7 @@ if(!empty($_POST)) {
 				<dt>写真など</dt>
 				<dd>
 					<input type="file" name="image" size="35" />
-					<?php if($error['image'] == 'type'): ?>
+					<?php if(isset($error['image']) && $error['image'] == 'type'): ?>
 						<p class="error">*　写真などは「.gif」または「.jpg」の画像を指定してください</p>
 					<?php endif; ?>
 					<?php if(!empty($error)): ?>
