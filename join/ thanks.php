30925d8584242c37baace8313609db0a5ce08ffe
ayktmr <ate.ayakotamura@gmail.com>
2020-05-15 11:11:48 +0900

    Initial commit

---
+++ join/ thanks.php
@@ -0,0 +1,28 @@
+
+
+<!doctype html>
+<html lang="ja">
+<head>
+<!-- Required meta tags -->
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+<!-- Bootstrap CSS -->
+<link rel="stylesheet" href="../css/style.css">
+
+<title>よくわかるPHPの教科書</title>
+</head>
+
+<body>
+<div id="wrap">
+	<div id="head">
+	<h1>会員登録</h1>
+	</div>
+	<div id="content">
+		<p>ユーザー登録が完了しました！</p>
+		<p><a href="../">ログインする</a></p>
+	</div>
+</div>
+
+</body>
+</html>
